fbc031379dbf3f70c201d7ebeea7517ca7a9d069
swittuthcoding <swittuthcoding@gmail.com>
2022-09-30 17:14:17 -0400

    edit layout for search and suggestion area

--- client/SearchForm.js
+++ client/SearchForm.js
@@ -4,27 +4,59 @@ export const SearchForm = () => {
   return (
     <Box
       sx={{
-        display: "flex",
-        flexDirection: "column",
+        display: "grid",
         width: "100%",
         height: "100%",
-        alignItems: "center",
-        justifyContent: "center",
+        gridTemplateAreas: `
+          "search-area"
+          "suggestion"
+        `,
+        gridTemplateRows: "5fr 5fr",
+        gridTemplateColumns: "1fr",
       }}
     >
-      <h1>Lots of dogs! 🐕</h1>
-      <p>See photos of your favorite dogs</p>
-      <FormControl
+      <Box
         sx={{
-          width: "50%",
+          gridArea: "search-area",
           display: "flex",
-          gap: "5px",
+          flexDirection: "column",
+          width: "100%",
+          height: "100%",
+          alignItems: "center",
+          justifyContent: "flex-end",
         }}
       >
-        <Input type="text" name="breed" placeholder="Enter a dog breed" />
-        <Button variant="outline">Fetch</Button>
-      </FormControl>
-      <div>{/* to display suggestion */}</div>
+        <h1>Lots of dogs! 🐕</h1>
+        <p>See photos of your favorite dogs</p>
+        <FormControl
+          sx={{
+            width: "50%",
+            display: "flex",
+            gap: "5px",
+          }}
+        >
+          <Input type="text" name="breed" placeholder="Enter a dog breed" />
+          <Button variant="outline">Fetch</Button>
+        </FormControl>
+      </Box>
+      <Box
+        sx={{
+          width: "100%",
+          gridArea: "suggestion",
+          display: "flex",
+          justifyContent: "center",
+          padding: "3px",
+        }}
+      >
+        <Box
+          sx={{
+            width: "50%",
+          }}
+        >
+          <Text>adsfdsa</Text>
+          <Text>asdfsad</Text>
+        </Box>
+      </Box>
     </Box>
   );
 };
