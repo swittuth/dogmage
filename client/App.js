6551b1d6547238a0cef2d5fbddc59b7dcab7323f
swittuthcoding <swittuthcoding@gmail.com>
2022-09-30 16:41:01 -0400

    configured styling

--- client/App.js
+++ client/App.js
@@ -1,10 +1,11 @@
 import { SearchForm } from "./SearchForm";
 import { AnimatedLogo } from "./AnimatedLogo";
 import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "./styling/theme";
 
 export const App = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       {/* <AnimatedLogo /> */}
       <SearchForm />
     </ChakraProvider>
--- client/SearchForm.js
+++ client/SearchForm.js
@@ -1,14 +1,23 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Box } from "@chakra-ui/react";
 
 export const SearchForm = () => {
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <h1>Lots of dogs! 🐕</h1>
       <p>See photos of your favorite dogs</p>
       <form>
         <input type="text" name="breed" placeholder="Enter a dog breed" />
         <Button variant="outline">Fetch</Button>
       </form>
-    </>
+    </Box>
   );
 };
