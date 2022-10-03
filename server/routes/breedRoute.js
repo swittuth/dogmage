cf711d3767a7f619a96ba117ecca4ccf7e2be190
swittuthcoding <swittuthcoding@gmail.com>
2022-10-03 10:26:22 -0400

    divided routes for backend point for suggestions so parsing doesn't need to be done

---
+++ server/routes/breedRoute.js
@@ -0,0 +1,53 @@
+import Router from "@koa/router";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const data = require("../models/breeds.json");
+
+const breedRoute = new Router({
+  prefix: "/dog/breed/suggestion",
+});
+
+breedRoute.get("/:breed/:type", async (ctx, next) => {
+  const suggestions = [];
+  const { breed, type } = ctx.params;
+
+  for (const dog in data.breed) {
+    if (dog.indexOf(breed) === 0) {
+      data.breed[dog].forEach((t) => {
+        if (t.indexOf(type) === 0) {
+          suggestions.push(dog + " " + t);
+        }
+      });
+    }
+  }
+
+  ctx.body = {
+    status: "200",
+    json: {
+      data: suggestions,
+    },
+  };
+});
+
+breedRoute.get("/:breed", async (ctx, next) => {
+  const suggestions = [];
+  const { breed } = ctx.params;
+
+  for (const dog in data.breed) {
+    if (dog.indexOf(breed) === 0) {
+      suggestions.push(dog);
+      data.breed[dog].forEach((t) => {
+        suggestions.push(dog + " " + t);
+      });
+    }
+  }
+
+  ctx.body = {
+    status: "200",
+    json: {
+      data: suggestions,
+    },
+  };
+});
+
+export { breedRoute };
